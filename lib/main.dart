59eb8e981fa5c2ec38d3fabcb33c561a9ae6c3bd
Raniel-Roque <raniel.roque0509@gmail.com>
2024-10-24 21:12:58 +0800

    Fixed Issues

--- lib/main.dart
+++ lib/main.dart
@@ -5,74 +5,116 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/Roque.jpg'),
-              ),
-              Text(
-                'Raniel Roque',
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 40.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                'FLUTTER DEVELOPER',
-                style: TextStyle(
-                  fontFamily: 'SourceSansPro',
-                  fontSize: 20.0,
-                  color: Colors.teal[100],
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 200.0,
-                child: Divider(
-                  color: Colors.teal.shade100,
-                ),
-              ),
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.teal,
-                  ),
-                  title: Text(
-                    '+63 998 379 0133',
-                    style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: 'SourceSansPro',
-                      fontSize: 20.0,
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Color.fromARGB(255, 100, 85, 158).withOpacity(0.35),
+                        Color.fromARGB(255, 68, 46, 158).withOpacity(1),
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
                     ),
                   ),
                 ),
               ),
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.email,
-                    color: Colors.teal,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 50.0,
+                    backgroundImage: AssetImage('images/Roque.jpg'),
                   ),
-                  title: Text(
-                    'raniel.roque0509@gmail.com',
+                  Text(
+                    'Raniel Roque',
                     style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: 'SourceSansPro',
-                      fontSize: 20.0,
+                      fontFamily: 'Pacifico',
+                      fontSize: 40.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
+                  SizedBox(
+                    height: 3,
+                  ),
+                  Text(
+                    'FLUTTER DEVELOPER',
+                    style: TextStyle(
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                      color: Colors.grey.shade300,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                    width: 200.0,
+                    child: Divider(
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      contentPadding: EdgeInsets.fromLTRB(20, 10, 10, 10),
+                      leading: Padding(
+                        padding: EdgeInsets.only(right: 30.0),
+                        child: Icon(
+                          Icons.phone,
+                          color: Color.fromARGB(255, 68, 46, 158),
+                        ),
+                      ),
+                      title: Text(
+                        '+63 998 379 0133',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 68, 46, 158),
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      contentPadding: EdgeInsets.fromLTRB(20, 10, 10, 10),
+                      leading: Padding(
+                        padding: EdgeInsets.only(right: 30.0),
+                        child: Icon(
+                          Icons.email,
+                          color: Color.fromARGB(255, 68, 46, 158),
+                        ),
+                      ),
+                      title: Text(
+                        'raniel.roque0509@gmail.com',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 68, 46, 158),
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
